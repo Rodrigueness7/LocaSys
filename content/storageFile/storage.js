0c69f4b482a69402d7e6205d5551f3f7b9874631
rodrigueness7 <alex.correarodrigues@hotmail.com>
2024-10-08 23:57:50 -0300

    Creating method to delete files from the upload folder.

--- content/storageFile/storage.js
+++ content/storageFile/storage.js
@@ -1,16 +1,38 @@
+const fs = require('fs')
 const multer = require('multer')
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads/')
-    }, 
-    filename: function(req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, uniqueSuffix+file.originalname)
+const upload = () => {
+    const storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, 'uploads/')
+        }, 
+        filename: function(req, file, cb) {
+            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+            cb(null, uniqueSuffix+file.originalname)
+        }
+    })
+    return multer({storage: storage})
+}
+
+const deleteFiles = (res) => {
+   const file = []
+   fs.readdir('./uploads', (error, files) => {
+    if(error) {
+        throw new Error('there was an error listing the files')
     }
-})
+    files.map(values => {
+        file.push(values)
+    })
+  
+    fs.unlink(`./uploads/${file[0]}`,(error) => {
+        if(error) {
+            throw new Error('Unable to delete file')
+        }
+        res.json({message:'File was delete successfully'})
+    })
+   })
+}
 
-const upload = multer({storage: storage})
 
 
-module.exports = upload
+module.exports = {upload, deleteFiles}
