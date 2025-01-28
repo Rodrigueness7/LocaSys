bc54124e7fa14348b544deb196b12a262f8461f4
rodrigueness7 <alex.correarodrigues@gmail.com>
2025-01-28 18:09:29 -0300

    Adding user checks to read data from logs.

--- routes/routeLog.js
+++ routes/routeLog.js
@@ -2,8 +2,9 @@ const express = require('express')
 const routeLog = express.Router()
 const controllerLog = require('../controller/controllerLog')
 const auth = require('../controller/auth')
+const {checkAcess} = require('../controller/checkAcess')
 
-routeLog.get('/findAllLog', auth.verifyToken, controllerLog.findAllLog)
-routeLog.get('/findLog', auth.verifyToken, controllerLog.findLog)
+routeLog.get('/findAllLog', auth.verifyToken, checkAcess('log'), controllerLog.findAllLog)
+routeLog.get('/findLog', auth.verifyToken, checkAcess('log'), controllerLog.findLog)
 
 module.exports = routeLog
